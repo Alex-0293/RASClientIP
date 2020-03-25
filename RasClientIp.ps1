841948c1fc2e7b7eb592b3547cd57402af90dab3
Alex-0293 <1928311@tuta.io>
2020-03-25 13:41:06 +0300

    Made work through Invoke-Command

--- RasClientIp.ps1
+++ RasClientIp.ps1
@@ -6,32 +6,74 @@
     PSVer:       4.0
     Author:     AlexK
 #>
+Import-Module AlexkUtils
+#requires -version 3
 
+#########################################################################
+function Get-Workdir () {
+    if ($PSScriptRoot -eq "") {
+        if ($PWD -ne "") {
+            $MyScriptRoot = $PWD
+        }        
+        else {
+            Write-Host "Where i am? What is my work dir?"
+        }
+    }
+    else {
+        $MyScriptRoot = $PSScriptRoot
+    }
+    return $MyScriptRoot
+}
+# Error trap
+trap {
+    Get-ErrorReporting $_    
+    exit 1
+}
+#########################################################################
 
 Clear-Host
-Function DigitToStrIPAddress($Digit9IPAddress) { # Формируем IP адрес из числового представления 
-    $bin = [convert]::ToString([int32]$Digit9IPAddress, 2).PadLeft(32, '0').ToCharArray()
-    $A = [convert]::ToByte($bin[0..7] -join "", 2)
-    $B = [convert]::ToByte($bin[8..15] -join "", 2)
-    $C = [convert]::ToByte($bin[16..23] -join "", 2)
-    $D = [convert]::ToByte($bin[24..31] -join "", 2)
-    return $($A, $B, $C, $D -join ".")
-} 
 
-$output = @() # Обнуляем хэш массив
-$ADUsers = get-aduser -filter * -Properties * | Where-Object { $_.msRASSavedFramedIPAddress -ne $null } #Выбираем всех пользователей с установленным RAS IP 
-Foreach ($AdUser in $ADUsers) {
-    $res = [pscustomobject]@{
-        Sam             = $AdUser.SamAccountName
-        RASIp           = DigitToStrIPAddress($AdUser.msRADIUSFramedIPAddress)
-        DialIn          = $AdUser.msnpallowdialin
-        PasswordExpired = $AdUser.PasswordExpired
-        Enabled         = $AdUser.Enabled
+$MyScriptRoot = Get-Workdir
+
+Get-Vars    "$MyScriptRoot\Vars.ps1"
+InitLogging $MyScriptRoot "Latest"
+
+$User = Get-VarFromFile $Global:GlobalKey1 $Global:APP_SCRIPT_ADMIN_Login
+$Pass = Get-VarFromFile $Global:GlobalKey1 $Global:APP_SCRIPT_ADMIN_Pass
+
+$Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $User, ($Pass | ConvertTo-SecureString  -AsPlainText -Force)
+
+$Global:PSSession1 = New-PSSession -ComputerName $Global:Computer  -Credential $Credentials 
+
+$output = Invoke-Command -Session $Global:PSSession1 -ScriptBlock {`
+    Function DigitToStrIPAddress($Digit9IPAddress) {
+        # Формируем IP адрес из числового представления 
+        $bin = [convert]::ToString([int32]$Digit9IPAddress, 2).PadLeft(32, '0').ToCharArray()
+        $A = [convert]::ToByte($bin[0..7] -join "", 2)
+        $B = [convert]::ToByte($bin[8..15] -join "", 2)
+        $C = [convert]::ToByte($bin[16..23] -join "", 2)
+        $D = [convert]::ToByte($bin[24..31] -join "", 2)
+        return $($A, $B, $C, $D -join ".")
+    } 
+    Import-Module ActiveDirectory
+    $output = @() # Инициализируем хэш массив
+    $ADUsers = get-aduser -filter * -Properties * | Where-Object { $_.msnpallowdialin -ne $null } #Выбираем всех пользователей с установленным RAS IP 
+    Foreach ($AdUser in $ADUsers) {
+        $res = [pscustomobject]@{
+            Sam             = $AdUser.SamAccountName
+            RASIp           = DigitToStrIPAddress($AdUser.msRADIUSFramedIPAddress)
+            DialIn          = $AdUser.msnpallowdialin
+            PasswordExpired = $AdUser.PasswordExpired
+            Enabled         = $AdUser.Enabled
+        }
+
+        $output += $res
     }
 
-    $output += $res
-}
+    
+    return $output | Sort-Object rasip | Where-Object { $_.enabled -eq $true } 
+    #$output | Sort-Object sam | Where-Object { $_.enabled -eq $true } | Export-Csv d:\rasip.csv
+} 
 
-$output | Sort-Object rasip | Where-Object { $_.enabled -eq $true } | Format-Table -AutoSize
-
-#$output | Sort-Object sam | Where-Object { $_.enabled -eq $true } | Export-Csv d:\rasip.csv
+#$output.gettype()
+$output | Select-Object Sam, RASIp, DialIn, PasswordExpired, Enabled | Format-Table -AutoSize
