9fd94633f3bd13a33654708f2df60c41e8875872
Alex-0293 <1928311@tuta.io>
2020-05-07 22:34:04 +0300

    Redesign logging.

--- SCRIPTS/RasClientIp.ps1
+++ SCRIPTS/RasClientIp.ps1
@@ -7,14 +7,15 @@
     .PARAMETER
     .EXAMPLE
 #>
-$MyScriptRoot = "C:\DATA\Projects\RASClientIP\SCRIPTS"
-$InitScript   = "C:\DATA\Projects\GlobalSettings\SCRIPTS\Init.ps1"
-
-. "$InitScript" -MyScriptRoot $MyScriptRoot
+Clear-Host
+$Global:ScriptName = $MyInvocation.MyCommand.Name
+$InitScript = "C:\DATA\Projects\GlobalSettings\SCRIPTS\Init.ps1"
+if (. "$InitScript" -MyScriptRoot (Split-Path $PSCommandPath -Parent)) { exit 1 }
 # Error trap
 trap {
     if ($Global:Logger) {
-        Get-ErrorReporting $_ 
+       Get-ErrorReporting $_
+        . "$GlobalSettings\$SCRIPTSFolder\Finish.ps1"  
     }
     Else {
         Write-Host "There is error before logging initialized." -ForegroundColor Red
@@ -22,7 +23,6 @@ trap {
     exit 1
 }
 ################################# Script start here #################################
-Clear-Host
 
 $User = Get-VarFromAESFile $Global:GlobalKey1 $Global:APP_SCRIPT_ADMIN_Login
 $Pass = Get-VarFromAESFile $Global:GlobalKey1 $Global:APP_SCRIPT_ADMIN_Pass
@@ -32,8 +32,7 @@ $Credentials = New-Object System.Management.Automation.PSCredential -ArgumentLis
 #$Global:PSSession1 = New-PSSession -ComputerName $Global:Computer  -Credential $Credentials 
 [array]$output = @() 
 $output = Invoke-Command -ComputerName  $Global:Computer  -Credential $Credentials  -ScriptBlock {`
-        Function DigitToStrIPAddress($Digit9IPAddress) {
-        # Формируем IP адрес из числового представления 
+    Function DigitToStrIPAddress($Digit9IPAddress) {
         $bin = [convert]::ToString([int32]$Digit9IPAddress, 2).PadLeft(32, '0').ToCharArray()
         $A = [convert]::ToByte($bin[0..7] -join "", 2)
         $B = [convert]::ToByte($bin[8..15] -join "", 2)
@@ -42,8 +41,8 @@ $output = Invoke-Command -ComputerName  $Global:Computer  -Credential $Credentia
         return $($A, $B, $C, $D -join ".")
     } 
     Import-Module ActiveDirectory
-    [array]$output = @()  # Инициализируем хэш массив
-    $ADUsers = get-aduser -filter * -Properties * | Where-Object { $_.msRADIUSFramedIPAddress -ne $null } #Выбираем всех пользователей с установленным RAS IP 
+    [array]$output = @()
+    $ADUsers = get-aduser -filter * -Properties * | Where-Object { $_.msRADIUSFramedIPAddress -ne $null }
     Foreach ($AdUser in $ADUsers) {
         $res = [pscustomobject]@{
             Sam             = $AdUser.SamAccountName
